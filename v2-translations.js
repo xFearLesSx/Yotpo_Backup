edcd2f0945665c48ac5a877a369ef2144085e3e6
JoelLemuelAbuga <jlabuga2k16@gmail.com>
2024-05-27 17:47:32 +0800

    may27

---
+++ v2-translations.js
@@ -0,0 +1,287 @@
+{
+    "en":{
+        "widget":{
+            "you":"You",
+            "me":"Me",
+            "vote_yes":"Yes",
+            "vote_no":"No",
+            "default_title":"Write your review title here!",
+            "default_review":"Write your review here!",
+            "default_name":"Enter your name",
+            "default_email":"Enter your email address",
+            "post":"Post",
+            "sort":"Sort",
+            "sort_choose":"Choose a sort option",
+            "sort_newest":"newest",
+            "sort_oldest":"oldest",
+            "sort_best":"best",
+            "sort_highest_rating":"Highest Rating",
+            "sort_lowest_rating":"Lowest Rating",
+            "sort_high_votes":"Most Votes",
+            "sort_low_votes":"Least Votes",
+            "sort_with_images":"With Pictures",
+            "all":"All",
+            "select":"Select",
+            "powered_by":"Reviews by YOTPO",
+            "powered_by_yotpo":"Powered by YOTPO",
+            "sign_in":"Login",
+            "sign_in_to_post":"Login to Post",
+            "sign_out":"Logout",
+            "helpful":"Was this review helpful?",
+            "helpful_answer":"Was this answer helpful?",
+            "loading":"Loading...",
+            "load_more":"Load More",
+            "load_more_alt_text":"Load more photos",
+            "title_error":"Missing review title",
+            "review_error":"Missing review text",
+            "question_error":"Missing question text",
+            "review_error_too_long":"Your review is too long",
+            "question_error_too_long":"Your question is too long",
+            "1st":"1<sup>st</sup>",
+            "2st":"2<sup>nd</sup>",
+            "3st":"3<sup>rd</sup>",
+            "pending_review":"Your review was sent successfully and is now waiting for our staff to publish it.",
+            "firstreview":" ",
+            "reviews":"Reviews",
+            "reviews_by":"Powered by",
+            "review":"Review",
+            "required":"",
+            "login_line_1":"Connect to see what people<br>you know say about<br>this product",
+            "login_line_2":"We will NEVER post anything without your permission",
+            "post_line_1":"Please connect one of your social networks in order to submit your review",
+            "post_line_2":"We will NEVER post anything without your permission",
+            "or_with":"or with",
+            "no-login":"Please sign in or enter a name and email address.",
+            "no-login-question":"Please enter your name and email address.",
+            "email_not_valid":"The e-mail address is not valid",
+            "no-name":"Please enter your name",
+            "no-email":"Please enter your email address",
+            "no-score":"Please enter a star rating for this review",
+            "connect_with":"Connect with",
+            "or":"or",
+            "email":"Email",
+            "facebook":"facebook",
+            "instagram":"Instagram",
+            "twitter":"twitter",
+            "share":"share",
+            "user_your_name":"Use your name",
+            "login_incentive":"Login to be like all those cool people",
+            "thank_you_for_posting":"Thank you for posting a review!",
+            "help_your_friends":"help your friends with their next shopping experience",
+            "be_a_friend":"We value your input. Share your review so everyone else can enjoy it too.",
+            "connect_one_omre_networks":"connect to one or more your social networks",
+            "connect_to_social_network":"connected to a social network.",
+            "connected_as":"You are connected as",
+            "share_one_omre_networks":"share to one or more of your social networks",
+            "january":"January",
+            "february":"February",
+            "march":"March",
+            "april":"April",
+            "may":"May",
+            "june":"June",
+            "july":"July",
+            "august":"August",
+            "september":"September",
+            "october":"October",
+            "november":"November",
+            "december":"December",
+            "verified_buyer_title":"Verified Buyer",
+            "verified_buyer_text":"A Verified Buyer is a user who has purchased the reviewed product through our store.",
+            "verified_reviewer_title":"Verified Reviewer",
+            "verified_reviewer_text":"A Verified Reviewer is a shopper who has confirmed an email address, or connected a social network with Yotpo, providing an added level of transparency and trust.",
+            "rating1":"Bad",
+            "rating2":"Poor",
+            "rating3":"Regular",
+            "rating4":"Good",
+            "rating5":"Gorgeous",
+            "testimonials_title":"These are real reviews <br> from real shoppers",
+            "testimonials_site_tab":"Site Reviews",
+            "testimonials_products_tab":"Product Reviews",
+            "be_the_first_to_review":"Be the first to leave a review for {product_name}!",
+            "no_product_reviews_on_site":"Currently there are no product reviews on this site!",
+            "on_rel":"On",
+            "site_review":"Write Site Review",
+            "store_owner":"Store Owner",
+            "product_review":"Write Product Review",
+            "badge_text":"certified reviews",
+            "home_page_link":"Go To Home Page",
+            "answers":"Answers",
+            "questions":"Questions",
+            "q_and_a_s":"Q&As",
+            "q":"Q",
+            "a":"A",
+            "write-question":"Write your question about",
+            "write-review":"write a review",
+            "required_fields":"Indicates a required field",
+            "ask-question":"ask a question",
+            "question-content":"Question",
+            "question-connect":"Enter your details",
+            "question-thankyou-title":"Thank you for posting a question!",
+            "question-thankyou-desc-confirmation":"Please click on the link in the confirmation email we just sent you to submit your question.<br/></br/>",
+            "question-thankyou-desc-published":"Your question will appear on the site once someone answers it.",
+            "answer-helpful":"Helpful",
+            "first-question":"Ask a question",
+            "score":"Score",
+            "title":"Title",
+            "per_page":"per page",
+            "what_is":"what is a",
+            "comments":"comments (%{total_comments})",
+            "total_answers":"answers (%{total_answers})",
+            "show_more_answers":"Show more answers",
+            "submit":"submit",
+            "shop_now":"Shop Now",
+            "buy_now":"Buy Now",
+            "add_to_cart":"Add to Cart",
+            "added_to_cart":"Added to cart",
+            "item_unavilable":"Item unavailable",
+            "continue_shopping":"Continue Shopping",
+            "proceed_to_checkout":"Proceed to Checkout",
+            "cancel_window":"Cancel",
+            "other_tagged_products":"Other tagged products",
+            "show_more_products":"SHOW MORE",
+            "show_less_products":"SHOW LESS",
+            "based_on_reviews":"based on %{total_reviews} reviews",
+            "based_on_review":"based on 1 review",
+            "read_more":"Read More",
+            "read_less":"Read Less",
+            "read_more_about":"Read more about",
+            "read_less_about":"Read less about",
+            "displaying":"Displaying %{type}",
+            "yotpo_home":"Yotpo home page",
+            "user_avatar":"User avatar",
+            "likes":"Likes",
+            "sold_in_the_last_month":"Sold in the last month",
+            "i_agree":"I agree to the",
+            "terms_conditions":"terms and conditions",
+             "errors":{
+                "empty":"Review's title & body can't be empty",
+                "score":"Please enter a star rating for this review",
+                "display_name":"Name field cannot be empty",
+                "email":"Invalid email",
+                "max_length":"Max length was exceeded",
+                "user-already-reviewed":"Your review has already been submitted.",
+                "mandatory_field":"Please fill out all of the mandatory (*) fields",
+                "open_question_answer_length":"Your answer must be at least %{min_length} characters in length",
+                "open_question_general":"One or more of your answers does not meet the required criteria"
+            },
+            "first":{
+                "empty":"Currently, there are no %{type} for this product.",
+                "empty_dedicated_page":"Currently, there are no %{type}.",
+                "be_the_first_review":"be the first to write a %{type}",
+                "be_the_first_question":"be the first to ask a %{type}",
+                "content_type":{
+                "review":"review",
+                "reviews":"reviews",
+                "question":"question",
+                "questions":"questions"
+                }
+            },
+            "placeholder":{
+                "title":"",
+                "review":"",
+                "question-content":"",
+                "user_your_name":"",
+                "email":""
+            },
+            "syndication":{
+                "reviewed_at":"Reviewed at"
+            },
+            "grouping":{
+                "grouped_review":"Reviewed on:"
+            },
+            "upload_videos":{
+                "button_title":"Add Your Own",
+                "caption_modal":{
+                "title":"Add a caption",
+                "invalid_type":"Files must be in JPG, PNG, GIF, MP4, MOV or FLV formats",
+                "invalid_size":"File size over 2GB"
+                },
+                "upload_error":"Upload failed, please try again"
+            },
+            "upload_photos":{
+                "button_title":"Add Your Photos",
+                "user_details_modal":{
+                "title":"Tell us about yourself",
+                "subtitle":"We'll never show your full name or email",
+                "name_placeholder":"Full Name",
+                "email_placeholder":"Email Address",
+                "submit":"Submit"
+                },
+                "thankyou_modal":{
+                "title":"Thank You!",
+                "title_secondary":"You×³re Awesome",
+                "subtitle":"Your posts will appear on-site",
+                "subtitle_secondary":"once theyâ€™re approved",
+                "close":"Close"
+                },
+                "upload_error_modal":{
+                "title":"Oops.",
+                "title_secondary":"Something went wrong",
+                "subtitle":"Check your connection or try again later",
+                "close":"Close"
+                },
+                "caption_modal":{
+                "title":"Add a caption to your photos",
+                "next_button":"Next",
+                "submit_button":"Submit",
+                "add_more_button":"Add more",
+                "caption_placeholder":"Add a captionâ€¦",
+                "invalid_type":"Files must be in JPG, PNG, or GIF formats",
+                "invalid_size":"File size over 15MB",
+                "invalid_image_dimensions":"Photos must be at least 400 x 400 px",
+                "invalid_image_file":"Sorry, we werenâ€™t able to upload your photo"
+                }
+            },
+            "product_bottomline":{
+                "related_fields":{
+                "see_more":"SEE MORE",
+                "see_less":"SEE LESS"
+                }
+            },
+            "filter_and_search":{
+                "filters_container":{
+                "header":"Filter Reviews",
+                "clear_all_btn_text":"Clear All",
+                "free_text_search_placeholder":"Search Reviews",
+                "scores_drop_down_button_value":"Rating",
+                "images_drop_down_button_value":"Images & Videos",
+                "images_drop_down_answer":"With Images & Videos"
+                },
+                "more-filters":"more filters",
+                "filters":"FILTERS",
+                "clear-all":"Clear All",
+                "only-with-images":"Only With Images",
+                "with-images-only":"Images Only",
+                "show":"Show",
+                "scores_element_value":"Rating",
+                "sort_drop_down_most_relevant_value":"Most Relevant"
+            },
+            "empty_search_results":{
+                "text":"Sorry, no reviews match your criteria. Clear or modify your filters and try again.",
+                "button_text":"Clear All Filters"
+            },
+            "search_in_progress":{
+                "text":"Updating Results"
+            }
+        },
+        "testimonials":{
+            "reviews_by":"Reviews by",
+            "yotpo_home":""
+        },
+        "star_distribution":{
+            "show_all_reviews":"Show all reviews"
+        },
+        "product_fields":{
+            "size":{
+                "tooltip":{
+                "small":"Marked Size Down",
+                "fit":"Marked Fit to Size",
+                "large":"Marked Size Up"
+                }
+            }
+        },
+        "shoppers_say":{
+            "more_reviews":"MORE REVIEWS"
+        }
+    }
+}
